78617a788099011c2c0a9c6b318b674d398bdbd3
Nash <axylschulz0@gmail.com>
2025-02-12 15:51:31 -0700

    first question almost

---
+++ e3/q1.hs
@@ -0,0 +1,31 @@
+
+
+import Prelude hiding (fst, snd, head, tail, take, drop, unzip, concat)
+
+fst :: [a] -> a
+fst (x:xs) = x
+
+snd :: [a] -> a
+snd (x:xs) = fst xs
+
+head :: [a] -> a
+head (x:xs) = x
+
+tail :: [a] -> a
+tail (x:xs)
+    | null xs = x
+    | otherwise = tail xs
+
+take :: Int -> [a] -> [a]
+take n (x:xs)
+    | n > 0 = x:take (n - 1) xs
+    | otherwise = []
+
+drop :: Int -> [a] -> [a]
+drop n (x:xs)
+    | n > 0 = drop (n-1) xs
+    | otherwise = xs
+
+unzip :: [(a,b)] -> ([a],[b])
+unzip [(a,b)] = ([a],[b])
+
